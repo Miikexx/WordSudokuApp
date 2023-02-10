5c700abd1a357a8414b8d940769a2244e8687b6a
ArmanL <ala264@sfu.ca>
2023-02-10 12:19:38 -0800

    Initial Version with How To Play and Main Menu by Hussain and Arman

---
+++ app/src/main/java/com/example/myapplication/HowToPlay.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HowToPlay extends AppCompatActivity {
+
+    Button howToPlayBackButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_play);
+
+
+        howToPlayBackButton = findViewById(R.id.HowToPlayBackButton);
+
+        howToPlayBackButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //Description: overrides onClick method in order to switch from main activity to how to play activity
+            public void onClick(View view){
+                Intent howToPlay = new Intent(HowToPlay.this, MainActivity.class);
+                startActivity(howToPlay);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button howToPlayButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //opens home screen of app and reveals its layout
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign id to how to play button
+        howToPlayButton = findViewById(R.id.HowToPlay);
+        //Adds a listener to howtoPlay button so that the user can click on it and see the game instructions
+        howToPlayButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    //Description: overrides onClick method in order to switch from main activity to how to play activity
+                    public void onClick(View view){
+                        Intent intent = new Intent(MainActivity.this, HowToPlay.class);
+                        startActivity(intent);
+            }
+        });
+    }
+}
